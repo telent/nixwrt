189554c040fff4d9d729ec212afbf1de0732c56a
Daniel Barlow <dan@telent.net>
2018-04-02 22:38:55 +0000

    patch device tree for working ethernet

--- nixwrt/kernel/default.nix
+++ nixwrt/kernel/default.nix
@@ -58,12 +58,14 @@ stdenv.mkDerivation rec {
       q_apply ${ledeSrc}/target/linux/generic/pending-4.9/
       q_apply ${ledeSrc}/target/linux/generic/hack-4.9/
       q_apply ${ledeSrc}/target/linux/ramips/patches-4.9/
+      cp ${ledeSrc}/target/linux/ramips/dts/GL-MT300A.dts gl-mt300.dts
       chmod -R +w .
     '';
 
     patches = [ ./kernel-ath79-wdt-at-boot.patch
                 ./kernel-lzma-command.patch
                 ./kernel-memmap-param.patch
+                ./kernel-dts-enable-eth0.patch
                 ];
 
     patchFlags = [ "-p1" ];
@@ -90,7 +92,7 @@ stdenv.mkDerivation rec {
       make vmlinux
       objcopy -O binary -R .reginfo -R .notes -R .note -R .comment -R .mdebug -R .note.gnu.build-id -S vmlinux vmlinux.stripped
       cc -o scripts/patch-dtb ${ledeSrc}/tools/patch-image/src/patch-dtb.c
-      cpp -nostdinc -x assembler-with-cpp -Iarch/mips/boot/dts -Iarch/mips/boot/dts/include -Iinclude/ -undef -D__DTS__  -o dtb.tmp ${ledeSrc}/target/linux/ramips/dts/GL-MT300A.dts
+      cpp -nostdinc -x assembler-with-cpp -I${ledeSrc}/target/linux/ramips/dts -Iarch/mips/boot/dts -Iarch/mips/boot/dts/include -Iinclude/ -undef -D__DTS__  -o dtb.tmp gl-mt300.dts
       scripts/dtc/dtc -O dtb -i${ledeSrc}/target/linux/ramips/dts/  -o vmlinux.dtb dtb.tmp
       scripts/patch-dtb vmlinux.stripped vmlinux.dtb
       rm -f vmlinux.stripped.lzma
