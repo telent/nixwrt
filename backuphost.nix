7a4c7671fa4962f6dff0a287500f56eeca26c98e
Daniel Barlow <dan@telent.net>
2018-04-04 15:56:56 +0000

    start to split up board config from app config

--- backuphost.nix
+++ backuphost.nix
@@ -15,49 +15,25 @@ let
     gcc = { abi = "32"; } ;
     bfdEmulation = "elf32btsmip";
   };
+  boardKernelConfig = import ./mt300a-kernel.nix lib;
   myKeys = (stdenv.lib.splitString "\n" ( builtins.readFile "/etc/ssh/authorized_keys.d/dan" ) );
   config = { pkgs, stdenv, ... } : {
     kernel = {
       # this pathname is relative to lede target/linux/
       defaultConfig = "ramips/mt7620/config-4.9";
-      overrideConfig = cfg : let adds = [
-        "CLKSRC_MMIO"
-        "CMDLINE_OVERRIDE"
-        "DEBUG_INFO"
-        "DEVTMPFS"
-        "EARLY_PRINTK"
-        "GENERIC_IRQ_IPI"
-        "IP_PNP"
-        "MIPS_CMDLINE_BUILTIN_EXTEND"
-        "MTD_CMDLINE_PART"
-        "MTD_PHRAM"
-        "NET_MEDIATEK_GSW_MT7620"
-        "NET_MEDIATEK_MT7620"
-        "PRINTK_TIME"
-        "SOC_MT7620"
-        "SQUASHFS"
-        "SQUASHFS_XZ"
-        "SQUASHFS_ZLIB"
-	"SWCONFIG"
-        "TMPFS"
-
-        "USB_COMMON"
-        "USB_STORAGE"
-        "USB_UAS"
-        "USB_ANNOUNCE_NEW_DEVICES"
-        "SCSI" "BLK_DEV_SD" "USB_PRINTER"
-        "PARTITION_ADVANCED"
-        "MSDOS_PARTITION" "EFI_PARTITION" "CMDLINE_PARTITION"
-        "EXT2_FS" "EXT3_FS" "EXT4_FS" "NTFS_FS"
-      ] ;
-      removes = ["MTD_ROOTFS_ROOT_DEV" "IMAGE_CMDLINE_HACK" "BLK_DEV_INITRD"];
-      others = {
-        "CPU_${lib.strings.toUpper platform.endian}_ENDIAN" = "y";
-        "CMDLINE" = builtins.toJSON "earlyprintk=serial,ttyS0 console=ttyS0,115200 panic=10 oops=panic init=/bin/init phram.phram=nixrootfs,0x2000000,11Mi root=/dev/mtdblock0 memmap=12M\$0x2000000 loglevel=8 rootfstype=squashfs";
-      };
-      in cfg // (lib.genAttrs adds (name: "y")) //
-                (lib.genAttrs removes (name: "n")) //
-                others;
+      overrideConfig = let
+        appConfig = cfg: let adds = [
+                            "USB_COMMON"
+                            "USB_STORAGE"
+                            "USB_UAS"
+                            "USB_ANNOUNCE_NEW_DEVICES"
+                            "SCSI" "BLK_DEV_SD" "USB_PRINTER"
+                            "PARTITION_ADVANCED"
+                            "MSDOS_PARTITION" "EFI_PARTITION"
+                            "EXT2_FS" "EXT3_FS" "EXT4_FS" "NTFS_FS"
+                           ];
+                           in cfg // (lib.genAttrs adds (name: "y"));
+        in (c: appConfig (boardKernelConfig c));
     };
     interfaces = {
       wired = {
---
+++ mt300a-kernel.nix
@@ -0,0 +1,33 @@
+lib : cfg :
+let adds = [
+  "CLKSRC_MMIO"
+  "CMDLINE_OVERRIDE"
+  "DEBUG_INFO"
+  "DEVTMPFS"
+  "EARLY_PRINTK"
+  "GENERIC_IRQ_IPI"
+  "IP_PNP"
+  "MIPS_CMDLINE_BUILTIN_EXTEND"
+  "MTD_CMDLINE_PART"
+  "MTD_PHRAM"
+  "NET_MEDIATEK_GSW_MT7620"
+  "NET_MEDIATEK_MT7620"
+  "PRINTK_TIME"
+  "SOC_MT7620"
+  "SQUASHFS"
+  "SQUASHFS_XZ"
+  "SQUASHFS_ZLIB"
+  "SWCONFIG"
+  "TMPFS"
+
+  "PARTITION_ADVANCED"
+  "CMDLINE_PARTITION"
+] ;
+removes = ["MTD_ROOTFS_ROOT_DEV" "IMAGE_CMDLINE_HACK" "BLK_DEV_INITRD"];
+others = {
+  "CPU_LITTLE_ENDIAN" = "y";
+  "CMDLINE" = builtins.toJSON "earlyprintk=serial,ttyS0 console=ttyS0,115200 panic=10 oops=panic init=/bin/init phram.phram=nixrootfs,0x2000000,11Mi root=/dev/mtdblock0 memmap=12M\$0x2000000 loglevel=8 rootfstype=squashfs";
+};
+in cfg // (lib.genAttrs adds (name: "y")) //
+          (lib.genAttrs removes (name: "n")) //
+          others
