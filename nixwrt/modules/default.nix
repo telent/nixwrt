64c198b382fc98a10d2e022141a318df3a3370e2
Daniel Barlow <dan@telent.net>
2018-06-19 08:31:14 +0000

    add busybox ntp applet when that module selected

--- nixwrt/modules/default.nix
+++ nixwrt/modules/default.nix
@@ -55,6 +55,7 @@
   ntpd = options: nixpkgs: self: super:
     with nixpkgs;
     lib.attrsets.recursiveUpdate super {
+      busybox.applets = super.busybox.applets ++ [ "ntpd" ];
       services.ntpd = {
         start = "/bin/ntpd -p ${options.host}";
       };
