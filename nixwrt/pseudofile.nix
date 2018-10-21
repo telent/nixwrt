a0e2454241a80eec4a1891c6803df2b36cdbc02a
Daniel Barlow <dan@telent.net>
2018-10-21 21:13:28 +0000

    pesudofile: allow directory creation

--- nixwrt/pseudofile.nix
+++ nixwrt/pseudofile.nix
@@ -1,9 +1,13 @@
 { lib, writeText, ...} : filename: prefix: specs:
-let defaults = { mode = "0444"; owner="root"; group="root"; };
+let defaults = { type = "f"; mode = "0444"; owner="root"; group="root"; };
 lines = lib.attrsets.mapAttrsToList
     (name: spec:
        let s = defaults // spec;
-           c = builtins.replaceStrings ["\n" "=" "\""] ["=0A" "=3D" "=22"] s.content; in
-           "/etc/${name} f ${s.mode} ${s.owner} ${s.group} echo -n \"${c}\" |qprint -d")
-           specs;
+           c = builtins.replaceStrings ["\n" "=" "\""] ["=0A" "=3D" "=22"] s.content;
+           line = "${prefix}${name} ${s.type} ${s.mode} ${s.owner} ${s.group}";
+           in if s.type=="f" then
+               "${line} echo -n \"${c}\" |qprint -d"
+             else
+               line)
+    specs;
 in writeText filename ( "${prefix} d 0755 root root\n" + (builtins.concatStringsSep "\n" lines))
