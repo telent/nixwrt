8eccb071fccba0107ee190745b8a41a3fb2b24de
Daniel Barlow <dan@telent.net>
2019-09-08 21:02:12 +0000

    fix misc crap

--- examples/arhcive.nix
+++ examples/arhcive.nix
@@ -1,14 +1,15 @@
-# Status May 2019: builds, may not boot, has not been tested recently
+# Status Sep 2019: builds, boots, Works On My Network
 
 { rsyncPassword ? "urbancookie"
 , myKeys ? "ssh-rsa AAAAATESTFOOBAR dan@example.org"
 , loghost ? "loghost"
 , sshHostKey ? "----FAKING RSA PRIVATE KEY----" }:
-let nixwrt = (import <nixwrt>) { targetBoard = "mt300nv2"; }; in
+let nixwrt = (import <nixwrt>) { targetBoard = "mt300n_v2"; }; in
 with nixwrt.nixpkgs;
 let
     baseConfiguration = {
       hostname = "arhcive";
+      webadmin = { allow = ["localhost" "192.168.8.0/24"]; };
       interfaces = {
         # this is set up for a GL.inet router, you'd have to edit it for another
         # target that has its LAN port somewhere else
@@ -18,16 +19,13 @@ let
         "eth0" = { } ;
         lo = { ipv4Address = "127.0.0.1/8"; };
       };
-      etc = {
-#        "resolv.conf" = { content = ( stdenv.lib.readFile "/etc/resolv.conf" );};
-      };
       users = [
         {name="root"; uid=0; gid=0; gecos="Super User"; dir="/root";
          shell="/bin/sh"; authorizedKeys = (stdenv.lib.splitString "\n" myKeys);}
         {name="store"; uid=500; gid=500; gecos="Storage owner"; dir="/srv";
          shell="/dev/null"; authorizedKeys = [];}
       ];
-      packages = [ pkgs.iproute ];
+      packages = [ ];
       filesystems = {} ;
       busybox = {
         # because I have empirically determined that
@@ -41,7 +39,6 @@ let
     wantedModules = with nixwrt.modules;
       [(_ : _ : _ : baseConfiguration)
        nixwrt.device.hwModule
-       (kexec {})
        (rsyncd { password = rsyncPassword; })
        (sshd { hostkey = sshHostKey ; })
        busybox
@@ -52,15 +49,19 @@ let
          options = "rw";
        })
        kernelMtd
+       haveged
        (switchconfig {
          name = "switch0";
          interface = "eth0";
          vlans = {"2" = "1 2 3 6t";  "3" = "0 6t"; };
         })
        (phram { offset = "0xa00000"; sizeMB = "5"; })
-       (syslogd { inherit loghost ; })
+       (syslog { inherit loghost ; })
        (ntpd { host = "pool.ntp.org"; })
-       (dhcpClient { interface = "eth0.2"; })
+       (dhcpClient {
+         resolvConfFile = "/run/resolv.conf";
+         interface = "eth0.2";
+        })
     ];
     in {
       firmware = nixwrt.firmware (nixwrt.mergeModules wantedModules);
