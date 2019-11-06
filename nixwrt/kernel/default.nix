bf5b4a3c3aecbef42a49b50f65042997a769082f
Daniel Barlow <dan@telent.net>
2019-11-06 00:15:46 +0000

    reindent

--- nixwrt/kernel/default.nix
+++ nixwrt/kernel/default.nix
@@ -17,33 +17,33 @@ let kconfigFile = writeText "nixwrt_kconfig"
           ));
     lib = stdenv.lib; in
 stdenv.mkDerivation rec {
-    name = "kernel";
+  name = "kernel";
 
-    hardeningDisable = ["all"];
-    nativeBuildInputs = [buildPackages.pkgs.bc buildPackages.stdenv.cc ];
-    CC = "${stdenv.cc.bintools.targetPrefix}gcc";
-    HOSTCC = "gcc";
-    CROSS_COMPILE = stdenv.cc.bintools.targetPrefix;
-    ARCH = "mips";              # use "mips" here for both mips and mipsel
-    dontStrip = true;
-    dontPatchELF = true;
-    phases = ["configurePhase" "buildPhase" "installPhase"
-    ];
+  hardeningDisable = ["all"];
+  nativeBuildInputs = [buildPackages.pkgs.bc buildPackages.stdenv.cc ];
+  CC = "${stdenv.cc.bintools.targetPrefix}gcc";
+  HOSTCC = "gcc";
+  CROSS_COMPILE = stdenv.cc.bintools.targetPrefix;
+  ARCH = "mips";              # use "mips" here for both mips and mipsel
+  dontStrip = true;
+  dontPatchELF = true;
+  phases = ["configurePhase" "buildPhase" "installPhase"
+  ];
 
-    configurePhase = ''
-      export KBUILD_OUTPUT=`pwd`
-      cp ${kconfigFile} .config
-      cp ${kconfigFile} .config.orig
-      ( cd ${sourceTree} && make V=1 olddefconfig )
-    '';
+  configurePhase = ''
+    export KBUILD_OUTPUT=`pwd`
+    cp ${kconfigFile} .config
+    cp ${kconfigFile} .config.orig
+    ( cd ${sourceTree} && make V=1 olddefconfig )
+  '';
 
-    KBUILD_BUILD_HOST = "nixwrt.builder";
-    buildPhase = ''
-      make -C ${sourceTree} vmlinux
-    '';
+  KBUILD_BUILD_HOST = "nixwrt.builder";
+  buildPhase = ''
+    make -C ${sourceTree} vmlinux
+  '';
 
-    installPhase = ''
-      cp vmlinux $out
-    '';
+  installPhase = ''
+    cp vmlinux $out
+  '';
 
-  }
+}
