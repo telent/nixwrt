cfa529a7e4998574f7f62aee84e249e0306bea29
Daniel Barlow <dan@telent.net>
2020-04-29 20:09:24 +0100

    add dnsmasq to test server (nol.nix)
    also fix the syntax errors

--- nol.nix
+++ nol.nix
@@ -57,6 +57,15 @@ in
     wantedBy = ["multi-user.target"];
   };
 
+  services.dnsmasq = {
+    enable = true;
+    servers = [ "8.8.8.8" "8.8.4.4" ];
+    extraConfig = ''
+    dhcp-range=10.0.0.22,10.0.0.200
+    interface=eth1
+'';
+  };
+  
   services.atftpd = {
     enable = true;
     extraOptions = ["--bind-address 10.0.0.1"];
@@ -64,9 +73,9 @@ in
   };
 
   users.users.root.openssh.authorizedKeys.keys = [
-"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+vxEVY7fZywS88tlgjqCVA9CQxChsN68G/UIEtXBoPI2/ufM4Esn6Mp113Ru7tM2Aarha2qxoibvXD8U8Gr88qU4CfS462rtzj6HrLNy+Uo/K/uVHaohe29qxqZjO2OZ8xwdXpf9QDthXf48mpKYghZWBfLAmDje90vPwZhHSMV6Z/X0Lv1zUQw6zd7kE56MkGwyT7379fmwJodQ+73KAlJsgpu8fxcBAcc3bgGrPMObCfvU21ilkMim10FXCUjfr9Rn/s+3QrKgsX6TuDaztLYEMG7eYmlcK1j8EWtSezLoBkvnmLD3dLKKpwl8lJ5u4HykA53DrAeJSD8z0stiH dan@noetbook"
-"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjSWxutcdErJKJ1Jslw4q1Xa9YPhUQF9dHQysE5ItAbDLp3fZciEZzQqvjFAs8LQziojCqDpfKqoNrQqEVHtjKYDivMqs7TZEv3W4DjGwh5nT1zPjwkXhrAaFp+vQM2EOICqq/wdJK3OmFG4GsF/UMUMsn5+4Qs8QXpxCwcIKFefRfLWcATF8VGgIsf2+ZvGNPVwY7jqe5oqtG3B/rAaCyRk/PkXDL1rmZyfkNKZvEs/f8pxGsgu4UCdz3TXJPOKEzWtCRcyshIFJ0uF0VAs48F/3NkfVI63sA5q83Jzlwj57FLDNfTPEeGPqHJXcjb1iu8ysJ1asWW0fxyHPH6hKd dan@loaclhost"
-"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDeWdZ3MDWUMwZm25q2kC4xsjUXDypOwKTolrj/BaG8by90rsytLSVuh+xOIrcxKQWezDMTbBjA6LJtXFHzJ+IIBpd+x6QMx0UdLGJ68q8cikg5rveLoqDgMd1TSGfqzEBCRfvLMe38hLNHB1g3hnzdWKgd0cEk7rNCwk/vlHFmH2vKN+XCx2pz9p0wq3z7aKIJnUype2jvwhFv9l3wtGsmBYYq1b3WOlgyD+oFZvWECdrKZ6BxNXb2/STZ8eLQ5izh+KLvUrcs6BrXqzgSDFVqJ40qE5/6Y5MIfxzJQF7ax4bV6G0WQaRQa6592E3G2geWjB6f5Au2WRbXeN0nWaM3 dan@tninkpad"
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+vxEVY7fZywS88tlgjqCVA9CQxChsN68G/UIEtXBoPI2/ufM4Esn6Mp113Ru7tM2Aarha2qxoibvXD8U8Gr88qU4CfS462rtzj6HrLNy+Uo/K/uVHaohe29qxqZjO2OZ8xwdXpf9QDthXf48mpKYghZWBfLAmDje90vPwZhHSMV6Z/X0Lv1zUQw6zd7kE56MkGwyT7379fmwJodQ+73KAlJsgpu8fxcBAcc3bgGrPMObCfvU21ilkMim10FXCUjfr9Rn/s+3QrKgsX6TuDaztLYEMG7eYmlcK1j8EWtSezLoBkvnmLD3dLKKpwl8lJ5u4HykA53DrAeJSD8z0stiH dan@noetbook"
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjSWxutcdErJKJ1Jslw4q1Xa9YPhUQF9dHQysE5ItAbDLp3fZciEZzQqvjFAs8LQziojCqDpfKqoNrQqEVHtjKYDivMqs7TZEv3W4DjGwh5nT1zPjwkXhrAaFp+vQM2EOICqq/wdJK3OmFG4GsF/UMUMsn5+4Qs8QXpxCwcIKFefRfLWcATF8VGgIsf2+ZvGNPVwY7jqe5oqtG3B/rAaCyRk/PkXDL1rmZyfkNKZvEs/f8pxGsgu4UCdz3TXJPOKEzWtCRcyshIFJ0uF0VAs48F/3NkfVI63sA5q83Jzlwj57FLDNfTPEeGPqHJXcjb1iu8ysJ1asWW0fxyHPH6hKd dan@loaclhost"
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDeWdZ3MDWUMwZm25q2kC4xsjUXDypOwKTolrj/BaG8by90rsytLSVuh+xOIrcxKQWezDMTbBjA6LJtXFHzJ+IIBpd+x6QMx0UdLGJ68q8cikg5rveLoqDgMd1TSGfqzEBCRfvLMe38hLNHB1g3hnzdWKgd0cEk7rNCwk/vlHFmH2vKN+XCx2pz9p0wq3z7aKIJnUype2jvwhFv9l3wtGsmBYYq1b3WOlgyD+oFZvWECdrKZ6BxNXb2/STZ8eLQ5izh+KLvUrcs6BrXqzgSDFVqJ40qE5/6Y5MIfxzJQF7ax4bV6G0WQaRQa6592E3G2geWjB6f5Au2WRbXeN0nWaM3 dan@tninkpad"
 
   ];
   users.mutableUsers = false;
@@ -76,17 +85,20 @@ in
     hostName = "nol"; # Define your hostname.
     useDHCP = true;
     enableIPv6 = true;
-
+    
     firewall = {
-      allowedUDPPorts = [ 69 # tftp
-                        ];
-
+      allowedUDPPorts = [
+        67 # bootps
+        68 # bootpc
+        69 # tftp
+      ];
+    };
     interfaces.eth1 = {
       useDHCP = false;
       ipv4.addresses = [ { address = "10.0.0.1"; prefixLength = 24;}];
     };
   };
-
+  
   environment.systemPackages = with pkgs; [
     wget vim rpPPPoE pciutils tcpdump
   ];
@@ -107,5 +119,4 @@ lcp-echo-failure 2
   services.openssh.passwordAuthentication = true;
 
   system.stateVersion = "17.09"; # Did you read the comment?
-
 }
