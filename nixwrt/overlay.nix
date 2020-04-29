0e627f6fdf3166d1553218424f776cd676a3b81b
Daniel Barlow <dan@telent.net>
2020-04-29 19:40:19 +0100

    link lzma statically
    This is to avoid "RPATH of binary ... contains a forbidden reference
    to /build/" messages

--- nixwrt/overlay.nix
+++ nixwrt/overlay.nix
@@ -9,13 +9,10 @@ in {
   # non-streaming ones.  See
   # https://sourceforge.net/p/squashfs/mailman/message/26599379/
 
-  lzma = super.buildPackages.stdenv.mkDerivation {
-    name = "lzma";
+  lzma = self.stdenv.mkDerivation {
+    pname = "lzma";
     version = "4.32.7";
-    # workaround for "forbidden reference to /tmp" message which will one
-    # day be fixed by a new patchelf release
-    # https://github.com/NixOS/nixpkgs/commit/cbdcc20e7778630cd67f6425c70d272055e2fecd
-    preFixup = ''rm -rf "$(pwd)" && mkdir "$(pwd)" '';
+    configureFlags = [ "--enable-static" "--disable-shared"];
     srcs = super.buildPackages.fetchurl {
       url = "https://tukaani.org/lzma/lzma-4.32.7.tar.gz";
       sha256 = "0b03bdvm388kwlcz97aflpr3ir1zpa3m0bq3s6cd3pp5a667lcwz";
