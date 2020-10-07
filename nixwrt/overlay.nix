c374ec2ac3827df070477ff5be62c69e2891ecf7
Daniel Barlow <dan@telent.net>
2020-10-07 14:40:26 +0100

    overlay.nix: alphabetize keys

--- nixwrt/overlay.nix
+++ nixwrt/overlay.nix
@@ -3,124 +3,17 @@ let
   stripped = p : p.overrideAttrs(o: { stripAllList = [ "bin" "sbin" ];});
 in {
 
-  # we need to build real lzma instead of using xz, because the lzma
-  # decoder in u-boot doesn't understand streaming lzma archives
-  # ("Stream with EOS marker is not supported") and xz can't create
-  # non-streaming ones.  See
-  # https://sourceforge.net/p/squashfs/mailman/message/26599379/
-
-  lzma = self.stdenv.mkDerivation {
-    pname = "lzma";
-    version = "4.32.7";
-    configureFlags = [ "--enable-static" "--disable-shared"];
-    srcs = super.buildPackages.fetchurl {
-      url = "https://tukaani.org/lzma/lzma-4.32.7.tar.gz";
-      sha256 = "0b03bdvm388kwlcz97aflpr3ir1zpa3m0bq3s6cd3pp5a667lcwz";
-    };
-  };
-
-  patchImage = self.stdenv.mkDerivation {
-    name = "patch-dtb";
-    src = self.fetchFromGitHub {
-      owner = "openwrt";
-      repo = "openwrt";
-      name = "openwrt-source" ;
-      rev = "a74095c68c4fc66195f7c4885171e4f1d9e5c5e6";
-      sha256 = "1kk4qvrp5wbrci541bjvb6lld60n003w12dkpwl18bxs9ygpnzlq";
-    };
-    configurePhase = "true";
-    buildPhase = ''
-      $CC -o patch-dtb tools/patch-image/src/patch-dtb.c
-      $CC -o patch-cmdline tools/patch-image/src/patch-cmdline.c
-    '';
-    installPhase = ''
-      mkdir -p $out/bin
-      cp patch-dtb patch-cmdline $out/bin
-    '';
-  };
-
-  kexectools = let ovr = o @ {patches ? []
-                              , buildInputs ? []
-                              , nativeBuildInputs ? []
-                              , ...}: {
-    patches = patches ++ [
-      (self.buildPackages.fetchpatch {
-        name = "mips-uimage.patch";
-        url = "https://github.com/telent/kexec-tools/compare/5aaa7b33d48b72e804509ebb47e250f1fc851a26.diff";
-        sha256 = "1anx4rbswc6928d4z0wsrqf8x02s3r10pz08x01q6g3dwfcn7q2x";
-      })
-    ];
-    buildInputs = buildInputs ++ [self.xz];
-  }; in super.kexectools.overrideAttrs ovr;
-
-  ubootMalta = self.pkgs.buildUBoot rec {
-    name = "uboot-malta";
-    defconfig = "qemu_mipsel_defconfig";
-    extraMeta.platforms = self.stdenv.lib.platforms.linux;# ["mipsel-unknown-linux-musl"];
-    filesToInstall = ["u-boot.bin"];
-  };
-
-  monit = stripped (super.monit.override { usePAM = false; useSSL = false; openssl = null; });
-
-  ppp = (super.ppp.override {
-    libpcap = null;
-  }).overrideAttrs (o : {
-     stripAllList = [ "bin" ];
-     buildInputs = [];
-     buildPhase = ''
-       runHook preBuild
-       make -C pppd USE_TDB= HAVE_MULTILINK= USE_EAPTLS= USE_CRYPT=y
-       make -C pppd/plugins/rp-pppoe
-       make -C pppd/plugins/pppol2tp
-       runHook postBuild;
-     '';
-     installPhase = ''
-      runHook preInstall
-      mkdir -p $out/bin $out/lib/pppd/2.4.8
-      cp pppd/pppd pppd/plugins/rp-pppoe/pppoe-discovery $out/bin
-      cp pppd/plugins/rp-pppoe/rp-pppoe.so $out/lib/pppd/2.4.8
-      cp pppd/plugins/pppol2tp/{open,pppo}l2tp.so $out/lib/pppd/2.4.8
-      runHook postInstall
-    '';
-    postFixup = "";
-  });
-
-  # temporary until patchelf#151 is applied upstream and nixpkgs gets new revision
-  patchelf = super.patchelf.overrideAttrs(o@{patches ? [], ...} :
-    let u = self.fetchurl {
-      url = "https://patch-diff.githubusercontent.com/raw/NixOS/patchelf/pull/151.diff" ;
-      sha256 = "12bzxf9ijqdkiqb9ljy4cra67hlmkyswd0yp88h8s06n3yc9d8gj";
-    }; in {
-      patches = patches ++ [u];
-  });
-
-  zlib = super.zlib.overrideAttrs (o: { dontStrip = false; });
-
-  swconfig =  stripped (self.callPackage ./pkgs/swconfig.nix { });
-
-  libpcap = super.libpcap.overrideAttrs (o: {
-    # tcpdump only wants the shared libraries, not all the
-    # headers and stuff
-    outputs = [ "lib" "out" ];
-    dontStrip = false;
-  });
-
-  tcpdump =super.tcpdump.overrideAttrs (o: { dontStrip = false; });
+  busybox = stripped (super.busybox.overrideAttrs (o: {
+    # busybox derivation has a postConfigure action conditional on useMusl that
+    # forces linking against musl instead of the system libc.  It does not appear
+    # to be required when musl *is* the system libc, and for me it seems to be
+    # picking up the wrong musl.  So let's get rid of it
+    postConfigure = "true";
+  }));
 
   coreutils =  super.coreutils.overrideAttrs (o: {
     # one of the tests fails under docker
     doCheck = false;
-  });
-
-  klogforward =
-    let ref = "47af2c6d451b9fec6ceff96002cbb938bd056f8a"; in
-    stripped (self.callPackage (builtins.fetchTarball "https://github.com/telent/klogforward/archive/${ref}.tar.gz" ) { } );
-
-  libnl = (super.libnl.override({  pythonSupport = false; })).overrideAttrs (o: {
-    outputs = [ "dev" "out" "man" ];
-    preConfigure = ''
-      configureFlagsArray+=(--enable-cli=no --disable-pthreads --disable-debug)
-    '';
   });
 
   dropbearSmall = stripped (super.dropbear.overrideAttrs (o: {
@@ -187,14 +80,6 @@ in {
         '';
       }));
 
-
-  xl2tpd = super.xl2tpd.overrideAttrs (o: {
-    postPatch = ''
-      substituteInPlace l2tp.h --replace /usr/sbin/pppd ${self.ppp}/bin/pppd
-
-   '';
-  });
-
   iprouteFull = super.iproute;
   iproute = stripped ((super.iproute.override {
     # db dep is only if we need arpd
@@ -207,15 +92,128 @@ in {
     '';
   }));
 
-  busybox = stripped (super.busybox.overrideAttrs (o: {
-    # busybox derivation has a postConfigure action conditional on useMusl that
-    # forces linking against musl instead of the system libc.  It does not appear
-    # to be required when musl *is* the system libc, and for me it seems to be
-    # picking up the wrong musl.  So let's get rid of it
-    postConfigure = "true";
-  }));
+  kexectools = let ovr = o @ {patches ? []
+                              , buildInputs ? []
+                              , nativeBuildInputs ? []
+                              , ...}: {
+    patches = patches ++ [
+      (self.buildPackages.fetchpatch {
+        name = "mips-uimage.patch";
+        url = "https://github.com/telent/kexec-tools/compare/5aaa7b33d48b72e804509ebb47e250f1fc851a26.diff";
+        sha256 = "1anx4rbswc6928d4z0wsrqf8x02s3r10pz08x01q6g3dwfcn7q2x";
+      })
+    ];
+    buildInputs = buildInputs ++ [self.xz];
+  }; in super.kexectools.overrideAttrs ovr;
+
+  klogforward =
+    let ref = "47af2c6d451b9fec6ceff96002cbb938bd056f8a"; in
+    stripped (self.callPackage (builtins.fetchTarball "https://github.com/telent/klogforward/archive/${ref}.tar.gz" ) { } );
+
+  libnl = (super.libnl.override({  pythonSupport = false; })).overrideAttrs (o: {
+    outputs = [ "dev" "out" "man" ];
+    preConfigure = ''
+      configureFlagsArray+=(--enable-cli=no --disable-pthreads --disable-debug)
+    '';
+  });
+
+  libpcap = super.libpcap.overrideAttrs (o: {
+    # tcpdump only wants the shared libraries, not all the
+    # headers and stuff
+    outputs = [ "lib" "out" ];
+    dontStrip = false;
+  });
+
+  # we need to build real lzma instead of using xz, because the lzma
+  # decoder in u-boot doesn't understand streaming lzma archives
+  # ("Stream with EOS marker is not supported") and xz can't create
+  # non-streaming ones.  See
+  # https://sourceforge.net/p/squashfs/mailman/message/26599379/
+
+  lzma = self.stdenv.mkDerivation {
+    pname = "lzma";
+    version = "4.32.7";
+    configureFlags = [ "--enable-static" "--disable-shared"];
+    srcs = super.buildPackages.fetchurl {
+      url = "https://tukaani.org/lzma/lzma-4.32.7.tar.gz";
+      sha256 = "0b03bdvm388kwlcz97aflpr3ir1zpa3m0bq3s6cd3pp5a667lcwz";
+    };
+  };
+
+  monit = stripped (super.monit.override { usePAM = false; useSSL = false; openssl = null; });
+
+  # temporary until patchelf#151 is applied upstream and nixpkgs gets new revision
+  patchelf = super.patchelf.overrideAttrs(o@{patches ? [], ...} :
+    let u = self.fetchurl {
+      url = "https://patch-diff.githubusercontent.com/raw/NixOS/patchelf/pull/151.diff" ;
+      sha256 = "12bzxf9ijqdkiqb9ljy4cra67hlmkyswd0yp88h8s06n3yc9d8gj";
+    }; in {
+      patches = patches ++ [u];
+  });
+
+  patchImage = self.stdenv.mkDerivation {
+    name = "patch-dtb";
+    src = self.fetchFromGitHub {
+      owner = "openwrt";
+      repo = "openwrt";
+      name = "openwrt-source" ;
+      rev = "a74095c68c4fc66195f7c4885171e4f1d9e5c5e6";
+      sha256 = "1kk4qvrp5wbrci541bjvb6lld60n003w12dkpwl18bxs9ygpnzlq";
+    };
+    configurePhase = "true";
+    buildPhase = ''
+      $CC -o patch-dtb tools/patch-image/src/patch-dtb.c
+      $CC -o patch-cmdline tools/patch-image/src/patch-cmdline.c
+    '';
+    installPhase = ''
+      mkdir -p $out/bin
+      cp patch-dtb patch-cmdline $out/bin
+    '';
+  };
+
+  ppp = (super.ppp.override {
+    libpcap = null;
+  }).overrideAttrs (o : {
+     stripAllList = [ "bin" ];
+     buildInputs = [];
+     buildPhase = ''
+       runHook preBuild
+       make -C pppd USE_TDB= HAVE_MULTILINK= USE_EAPTLS= USE_CRYPT=y
+       make -C pppd/plugins/rp-pppoe
+       make -C pppd/plugins/pppol2tp
+       runHook postBuild;
+     '';
+     installPhase = ''
+      runHook preInstall
+      mkdir -p $out/bin $out/lib/pppd/2.4.8
+      cp pppd/pppd pppd/plugins/rp-pppoe/pppoe-discovery $out/bin
+      cp pppd/plugins/rp-pppoe/rp-pppoe.so $out/lib/pppd/2.4.8
+      cp pppd/plugins/pppol2tp/{open,pppo}l2tp.so $out/lib/pppd/2.4.8
+      runHook postInstall
+    '';
+    postFixup = "";
+  });
 
   # we had trouble building rsync with acl support
   rsync = stripped (super.rsync.override { enableACLs = false; } );
 
+  swconfig =  stripped (self.callPackage ./pkgs/swconfig.nix { });
+
+  tcpdump =super.tcpdump.overrideAttrs (o: { dontStrip = false; });
+
+  ubootMalta = self.pkgs.buildUBoot rec {
+    name = "uboot-malta";
+    defconfig = "qemu_mipsel_defconfig";
+    extraMeta.platforms = self.stdenv.lib.platforms.linux;# ["mipsel-unknown-linux-musl"];
+    filesToInstall = ["u-boot.bin"];
+  };
+
+  xl2tpd = super.xl2tpd.overrideAttrs (o: {
+    postPatch = ''
+      substituteInPlace l2tp.h --replace /usr/sbin/pppd ${self.ppp}/bin/pppd
+
+   '';
+  });
+
+  zlib = super.zlib.overrideAttrs (o: { dontStrip = false; });
 }
