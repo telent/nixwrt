0c7feaf7f30f5f2b9dab21d20c70ad2b14e6e7c5
Daniel Barlow <dan@telent.net>
2021-11-01 21:10:29 +0000

    rename allConfig to configuration

--- default.nix
+++ default.nix
@@ -2,12 +2,12 @@ let device = import <nixwrt-device>;
     nixwrt = (import <nixwrt>) { inherit (device) endian; }; in
 with nixwrt.nixpkgs;
 let
-  allConfig = nixwrt.mergeModules modules;
   modules = import <nixwrt-config> { inherit device lib nixwrt; };
+  configuration = nixwrt.mergeModules modules;
 in rec {
-  emulator = nixwrt.emulator allConfig;
+  emulator = nixwrt.emulator configuration;
 
-  firmware = nixwrt.firmware allConfig;
+  firmware = nixwrt.firmware configuration;
 
   # phramware generates an image which boots from the "fake" phram mtd
   # device - required if you want to boot from u-boot without
@@ -16,5 +16,5 @@ in rec {
     let phram_ = (nixwrt.modules.phram {
           offset = "0xa00000"; sizeMB = "7";
         });
-    in nixwrt.firmware (nixwrt.mergeModules [allConfig phram_]);
+    in nixwrt.firmware (nixwrt.mergeModules [configuration phram_]);
 }
