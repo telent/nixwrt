2494e0ed64fdfa1863a861c9a7cffcd9a2234d18
Daniel Barlow <dan@telent.net>
2021-11-14 17:40:09 +0000

    build strace (requires deselecting libunwind)

--- nixwrt/overlay.nix
+++ nixwrt/overlay.nix
@@ -208,6 +208,8 @@ in {
 
   svc = self.callPackage ./services {};
 
+  strace = super.strace.override { libunwind = null; };
+
   swconfig =  stripped (self.callPackage ./pkgs/swconfig.nix { });
 
   tcpdump =super.tcpdump.overrideAttrs (o: { dontStrip = false; });
