d03a28b66c5de1f3e68e05f6754895699dfc8b64
Daniel Barlow <dan@telent.net>
2021-11-18 18:54:22 +0000

    make each service its own file

--- nixwrt/services/default.nix
+++ nixwrt/services/default.nix
@@ -1,9 +1,3 @@
-{svc, iproute} : {
-  netdevice = { ifname } : svc {
-    name = "${ifname}";
-    outputs = ["ready"];
-    config = { packages = [ iproute ]; };
-    start = "${iproute}/bin/ip link set up dev ${ifname}; setstate ready true";
-    stop = "${iproute}/bin/ip link set down dev ${ifname}";
-  };
+{svc, callPackage} : {
+  netdevice = callPackage ./netdevice.nix {};
 }
---
+++ nixwrt/services/netdevice.nix
@@ -0,0 +1,8 @@
+{ iproute, svc }:
+ifname : svc {
+  name = "${ifname}";
+  outputs = ["ready"];
+  config = { packages = [ iproute ]; };
+  start = "${iproute}/bin/ip link set up dev ${ifname}; setstate ready true";
+  stop = "${iproute}/bin/ip link set down dev ${ifname}";
+}
