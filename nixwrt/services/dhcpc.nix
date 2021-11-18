d94f93927d9bebe03bf46f70fd9dec211fd21cbe
Daniel Barlow <dan@telent.net>
2021-11-18 23:27:18 +0000

    dhcp: set state when lease acquired

--- nixwrt/services/dhcpc.nix
+++ nixwrt/services/dhcpc.nix
@@ -5,15 +5,22 @@
 } :
 let
   ifname = interface.name;
+  serviceName = "dhcpc-${interface.name}";
   dhcpscript = ''
     #!/bin/sh
     dev=${ifname}
+    . ${interface.statefns} ${serviceName}
     ip() { ${iproute}/bin/ip  $* ; }
     deconfig(){
       ip addr flush dev $dev
+      rmstate address
     }
     bound(){
       ip addr replace $ip/$mask dev $dev ;
+      setstate ready true
+      setstate address $ip
+      prefix=$(busybox ipcalc -p $subnet)
+      setstate prefix ''${prefix#PREFIX=}
       # ip route add 0.0.0.0/0 via $router;
     }
     case $1 in
@@ -24,17 +31,21 @@ let
         bound
         ;;
       *)
-        echo unrecognised command $1
+        echo $0: unrecognised subcommand $1
         ;;
     esac
     '';
 in svc rec {
-  name = "dhcpc-${interface.name}";
+  name = serviceName;
   config = {
-    busybox.applets = [ "udhcpc" ];
+    busybox = {
+      applets = [ "udhcpc" "ipcalc" ];
+      config.FEATURE_IPCALC_FANCY = "y";
+    };
   };
   pid = "/run/${name}.pid";
-  start = "/bin/udhcpc -x hostname:${hostname} -i ${ifname} -p ${pid}  -s '${writeScriptBin "dhcpscript" dhcpscript}/bin/dhcpscript'";
+  start = "/bin/udhcpc -f -F ${hostname} -x hostname:${hostname} -i ${ifname} -p ${pid}  -s '${writeScriptBin "dhcpscript" dhcpscript}/bin/dhcpscript'";
+  foreground = true;
   depends = [ interface.ready ];
-  outputs = [ "ready"];
+  outputs = [ "ready" "address" "prefix"];
 }
